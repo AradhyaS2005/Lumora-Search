5f0897066bdb6b6f636ae7ba2c9fe33b8a5ddfd5
AradhyaS2005 <aradhyasoni.udr@gmail.com>
2026-07-08 00:50:12 +0530

    improved look of search-bar and some improvement of look in the main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,18 @@ import SearchBar from "./components/SearchBar";
 
 export default function Home() {
   return (
-    <main className="flex-1 flex flex-col items-center justify-center">
-      <div className="max-w-7xl mx-auto px-6">
-        <p className="text-gray-400 mb-6">
-        Find where to stream movies & TV shows
-      </p>
-      <SearchBar />
+    <main className="flex-1 flex items-center justify-center">
+      <div className="text-center max-w-3xl px-6">
+        <h1 className="text-6xl font-bold mb-6">
+          Find Your Next Stream
+        </h1>
+
+        <p className="text-xl text-gray-400 mb-10">
+          Search across streaming platforms and instantly discover
+          where your favorite movies and TV shows are available.
+        </p>
+
+        <SearchBar />
       </div>
     </main>
   );
